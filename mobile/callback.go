41e51580263aee50e58c0b4cefb8e186e4ac0c24
Andrey Meshkov <am@adguard.com>
2019-04-10 15:53:39 +0300

    Extend DNSRequestProcessedEvent struct

--- mobile/callback.go
+++ mobile/callback.go
@@ -4,6 +4,8 @@ import (
 	"sync"
 	"time"
 
+	"github.com/miekg/dns"
+
 	"github.com/AdguardTeam/dnsproxy/proxy"
 )
 
@@ -13,10 +15,18 @@ var dnsRequestProcessedListenerGuard sync.Mutex             // nolint
 
 // DNSRequestProcessedEvent
 type DNSRequestProcessedEvent struct {
-	RawDNSMessage string // raw DNS message (it contains both Question and Answer sections)
-	Elapsed       int    // elapsed time in milliseconds
-	UpstreamAddr  string // Address of the upstream used to resolve
-	Error         string // If not empty, contains the error text (occurred while processing the DNS query)
+	Domain string // Queried domain name
+	Type   string // Query type
+
+	StartTime    int64  // Time when dnsproxy started processing request (epoch in milliseconds)
+	Elapsed      int    // Time elapsed on processing
+	Answer       string // DNS Answers string representation
+	UpstreamAddr string // Address of the upstream used to resolve
+
+	BytesSent     int // Number of bytes sent
+	BytesReceived int // Number of bytes received
+
+	Error string // If not empty, contains the error text (occurred while processing the DNS query)
 }
 
 // DNSRequestProcessedListener is a callback interface that can be configured from by the client library
@@ -41,15 +51,38 @@ func handleDNSResponse(d *proxy.DNSContext, err error) {
 	}
 
 	e := DNSRequestProcessedEvent{}
-	if d.Res != nil {
-		e.RawDNSMessage = d.Res.String()
-	} else {
-		e.RawDNSMessage = d.Req.String()
-	}
+
+	// Query properties
+	e.Domain = d.Req.Question[0].Name
+	e.Type = dns.Type(d.Req.Question[0].Qtype).String()
+
+	// Set time/elapsed
+	e.StartTime = d.StartTime.UnixNano() / int64(time.Millisecond)
 	e.Elapsed = int(time.Since(d.StartTime) / time.Millisecond)
+
+	// Send/received
+	e.BytesSent = d.Req.Len()
+	if d.Res != nil {
+		e.BytesReceived = d.Res.Len()
+	}
+
+	// Set answer
+	if d.Res != nil && len(d.Res.Answer) > 0 {
+		s := ""
+		for i := 0; i < len(d.Res.Answer); i++ {
+			if d.Res.Answer[i] != nil {
+				s += d.Res.Answer[i].String() + "\n"
+			}
+		}
+		e.Answer = s
+	}
+
+	// Upstream
 	if d.Upstream != nil {
 		e.UpstreamAddr = d.Upstream.Address()
 	}
+
+	// Error
 	if err != nil {
 		e.Error = err.Error()
 	}

15f16ef96502ad68b238c79ff041324632387c4b
Andrey Meshkov <am@adguard.com>
2020-09-09 23:59:12 +0300

    Test that QUIC session is reused

--- upstream/upstream_quic_test.go
+++ upstream/upstream_quic_test.go
@@ -3,6 +3,7 @@ package upstream
 import (
 	"testing"
 
+	"github.com/lucas-clemente/quic-go"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -12,8 +13,18 @@ func TestUpstreamDOQ(t *testing.T) {
 	u, err := AddressToUpstream(address, Options{InsecureSkipVerify: true})
 	assert.Nil(t, err)
 
+	uq := u.(*dnsOverQUIC)
+	var sess quic.Session
+
 	// Test that it responds properly
 	for i := 0; i < 10; i++ {
 		checkUpstream(t, u, address)
+
+		if sess == nil {
+			sess = uq.session
+		} else {
+			// This way we test that the session is properly reused
+			assert.True(t, sess == uq.session)
+		}
 	}
 }

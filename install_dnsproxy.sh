c8aa4b735ef002bc3b41e98216ebb920c7335630
LongQT-sea <long72073801@gmail.com>
2025-04-06 10:46:09 +0700

    Update install_dnsproxy.sh

--- install_dnsproxy.sh
+++ install_dnsproxy.sh
@@ -7,8 +7,8 @@ REPO_NAME="dnsproxy"
 ARGS="--listen 0.0.0.0 --listen :: --port 5353 \
     --bootstrap 8.8.8.8 --bootstrap 2606:4700:4700::1111 \
     --fallback 9.9.9.9 --fallback 2620:fe::9 \
-    --upstream https://dns.adguard.com/dns-query --upstream quic://dns.adguard.com \
     --upstream [/www.googleadservices.com/]8.8.8.8 --upstream [/ad.doubleclick.net/]8.8.8.8 \
+    --upstream https://dns.adguard.com/dns-query --upstream quic://dns.adguard.com \
     --ratelimit 500 --cache --cache-optimistic --cache-size 2097152  --output /dev/null"
 
 # Fetch the latest release tag from GitHub API
